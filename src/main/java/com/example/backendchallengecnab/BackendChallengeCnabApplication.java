57f2a7b678f68f5264d17d9eec27a74932221fe2
Eriklps <erikdslopes@gmail.com>
2024-01-02 08:49:27 -0300

    Initial commit

---
+++ src/main/java/com/example/backendchallengecnab/BackendChallengeCnabApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendchallengecnab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallengeCnabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChallengeCnabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendchallengecnab/BackendChallengeCnabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendchallengecnab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallengeCnabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
