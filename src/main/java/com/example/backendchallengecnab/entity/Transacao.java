d1d80f9caaa7272a130360fe2e56579b33b43bab
Eriklps <erikdslopes@gmail.com>
2024-01-04 08:38:52 -0300

    implemented TipoTransacao

--- src/main/java/com/example/backendchallengecnab/entity/Transacao.java
+++ src/main/java/com/example/backendchallengecnab/entity/Transacao.java
@@ -6,17 +6,21 @@ import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+
 public record Transacao(
-        Long id,
+        @Id Long id,
         Integer tipo,
         Date data,
         BigDecimal valor,
         Long cpf,
         String cartao,
         Time hora,
-        String donoDaLoja,
-        String nomeDaLoja) {
+        @Column("dono_loja") String donoDaLoja,
+        @Column("nome_loja") String nomeDaLoja) {
 
+    // Wither Pattern
     public Transacao withValor(BigDecimal valor) {
         return new Transacao(
                 id, tipo, data, valor, cpf,
